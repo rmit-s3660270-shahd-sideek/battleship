864b9a3e2ac811654b2d85680488ae2fe147a06f
Shahd Sideek <s3660270@student.rmit.edu.au>
2018-09-27 17:19:50 +1000

    Task A done

--- src/player/RandomGuessPlayer.java
+++ src/player/RandomGuessPlayer.java
@@ -11,41 +11,85 @@ import world.World;
  */
 public class RandomGuessPlayer implements Player{
 
+   World world;
+
     @Override
     public void initialisePlayer(World world) {
-        // To be implemented.
-    } // end of initialisePlayer()
+    	
+    	this.world = world;
+    	for(World.ShipLocation l  : world.shipLocations) {
+    		System.out.println(l);
+    	}
+        
+    } 
 
     @Override
     public Answer getAnswer(Guess guess) {
-        // To be implemented.
+    	
+    	Answer answer = new Answer();
+    	
+    	for(World.ShipLocation l : this.world.shipLocations) {
+    		
+    		for(World.Coordinate c : l.coordinates) {
+    			if(c.row == guess.row && c.column == guess.column) {
+    				answer.isHit = true;
+    				l.coordinates.remove(c);
+    				if(l.coordinates.size() == 0) {
+    					answer.shipSunk = l.ship;
+    				}
+    				break;
+    			}
+    		}
+    		if(answer.isHit) {
+    			if(answer.shipSunk != null) {
+    				this.world.shipLocations.remove(l);
+    				System.out.println("Ship Sunk");
+    			}
+    			break;
+    		}
+    	}
+        
 
-        // dummy return
-        return null;
+        
+        return answer;
     } // end of getAnswer()
 
 
     @Override
     public Guess makeGuess() {
-        // To be implemented.
+       Guess guess = new Guess();
+       int row;
+       int column;
+       
+       Random random = new Random();
+       
+       guess.row = random.nextInt(10);
+       guess.column = random.nextInt(10);
+       
+       world.updateShot(guess);
 
         // dummy return
-        return null;
+        return guess;
     } // end of makeGuess()
 
 
     @Override
     public void update(Guess guess, Answer answer) {
-        // To be implemented.
+       //no need to do this for task A
+    	
     } // end of update()
 
 
     @Override
     public boolean noRemainingShips() {
-        // To be implemented.
+       
+    	if(world.shipLocations.size()==0) {
+    		return true;
+    	}
 
         // dummy return
-        return true;
+        return false;
+       
     } // end of noRemainingShips()
 
 } // end of class RandomGuessPlayer
